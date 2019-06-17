29e883ccf19c0da28d3852b1b4a468e09d18837a
Manthan Patel <14mcei14@nirmauni.ac.in>
2019-06-17 11:07:30 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,5 +1,54 @@
 (function (window,Handsontable) {
     const privatePool = new WeakMap();
+
+    // Event for `keydown` event. Add condition after delay of 200 ms which is counted from time of last pressed key.
+    var debounceFn = Handsontable.helper.debounce(function (colIndex, event) {
+        var filtersPlugin = hot.getPlugin('filters');
+        filtersPlugin.removeConditions(colIndex);
+        filtersPlugin.addCondition(colIndex, 'contains', [event.realTarget.value]);
+        filtersPlugin.filter();
+    }, 200);
+    
+    var addEventListeners = function (input, colIndex) {
+        input.addEventListener('keydown', function(event) {
+        debounceFn(colIndex, event);
+        });
+    };
+    
+    // Build elements which will be displayed in header.
+    var getInitializedElements = function(colIndex) {
+        var div = document.createElement('div');
+        var input = document.createElement('input');
+    
+        div.className = 'filterHeader';
+    
+        addEventListeners(input, colIndex);
+    
+        div.appendChild(input);
+    
+        return div;
+    };
+    
+    // Add elements to header on `afterGetColHeader` hook.
+    var addInput = function(col, TH) {
+        // Hooks can return value other than number (for example `columnSorting` plugin use this).
+        if (typeof col !== 'number') {
+        return col;
+        }
+    
+        if (col >= 0 && TH.childElementCount < 2) {
+            TH.appendChild(getInitializedElements(col));
+        }
+    };
+    
+    // Deselect column after click on input.
+    var doNotSelectColumn = function (event, coords) {
+        if (coords.row === -1 && event.realTarget.nodeName === 'INPUT') {
+        event.stopImmediatePropagation();
+        this.deselectCell();
+        }
+    };
+
     window.customAutoCompleteEditor = Handsontable.editors.HandsontableEditor.prototype.extend();
     customAutoCompleteEditor.prototype.init = function() {
         Handsontable.editors.HandsontableEditor.prototype.init.apply(this, arguments);
@@ -72,7 +121,6 @@
             const { filteringCaseSensitive, allowHtml } = this.cellProperties;
             const query = this.query;
             console.info(query);
-            debugger;
             let cellValue = Handsontable.helper.stringify(value);
             let indexOfMatch;
             let match;
@@ -103,7 +151,11 @@
             }
     
             return trimDropdown ? columnWidth : columnWidth + 15;
-          }
+          },
+          colHeaders: true,
+          filters: true,
+          afterGetColHeader: addInput,
+          beforeOnCellMouseDown: doNotSelectColumn
         });
     
         // Add additional space for autocomplete holder
