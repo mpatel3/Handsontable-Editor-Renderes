b4df2584da056f738365d6cf078c4df051688790
Manthan Patel <14mcei14@nirmauni.ac.in>
2019-06-28 19:41:04 +0530

    Update tracker_start.js

--- trackerModule/tracker_start.js
+++ trackerModule/tracker_start.js
@@ -1,3 +1,5 @@
 import * as trackerModule from './tracker_module';
+import * as adminTrackerModule from './admin_tracker_module';
 
 window.ES6MangoSpring.trackerModule = trackerModule.default;
+window.ES6MangoSpring.adminTrackerModule = adminTrackerModule.default;
