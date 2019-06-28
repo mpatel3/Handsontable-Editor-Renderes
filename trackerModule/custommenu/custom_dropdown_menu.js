2ae0f1050f25595fd9e6a989cf7b2b7db8e49456
Manthan Patel <14mcei14@nirmauni.ac.in>
2019-06-28 19:45:57 +0530

    Update custom_dropdown_menu.js

--- trackerModule/custommenu/custom_dropdown_menu.js
+++ trackerModule/custommenu/custom_dropdown_menu.js
@@ -1,6 +1,7 @@
 /* eslint prefer-rest-params: 0 */
 /* eslint no-param-reassign: 0 */
 /* eslint dot-notation: 0 */
+/* object-shorthand:0 */
 import utils from '../../utils';
 
 const customDropDownMenu = () => {
@@ -12,14 +13,17 @@ const customDropDownMenu = () => {
      * @colInfo - column configuration - Array of objects.
      * @isArchiveView - Is Archive view.
      */
-    const createCustomDropDownMenu = (hotInstance, colHeaders, colInfo, isArchiveView = false) => {
+    const createCustomDropDownMenu = (hotInstance, colHeaders, colInfo, permissions, isArchiveView = false) => {
         const dropDownMenu = {};
         dropDownMenu['customize'] = {
-            name: '<i class="far fa-users-cog"></i> Customize Column',
+            name: '<i class="far fa-pencil-alt"></i> Edit Column Properties',
             disable: {},
+            hidden: function () {
+              return !permissions.is_creator_or_admin;
+            },
             callback: function (key, selection) {
               const colNumber = selection[0].end.col,
-                    colId = this.getCellMeta(0, colNumber).prop,
+                    colId = this.getCellMeta(0, colNumber).colId,
                     trackerId = document.querySelector('input[name="tracker_id"]').value,
                     url = `/user/v2/tracker/tracker_columns/${colId}/edit?tracker_id=${trackerId}`,
                     content = '<div id="initTrackerColumnFormWrpp"></div>';
@@ -37,11 +41,36 @@ const customDropDownMenu = () => {
               });
             },
         };
+        dropDownMenu['change_col_type'] = {
+            name: '<i class="far fa-shapes"></i> Change Column Type',
+            disable: {},
+            hidden: function () {
+              return !permissions.is_creator_or_admin;
+            },
+            callback: function (key, selection) {
+              const colNumber = selection[0].end.col,
+                    colMetaData = this.getCellMeta(0, colNumber),
+                    trackerId = document.querySelector('input[name="tracker_id"]').value,
+                    content = '<div id="changeColumnTypeWrpp"></div>',
+                    data = { 
+                        data_type: colMetaData.data_type,
+                        colMetaData: colMetaData,
+                    };
+
+              ES6MangoSpring.trackerModule.tracker().initFancybox(null, content, 650, () => {
+                amplify.publish('initChangeColumnType', data);
+              });  
+            },
+        };
         dropDownMenu['col_left'] = {
             name: '<i class="fal fa-arrow-to-left"></i> Insert column left',
             disabled: function () {
               const selectedRows = this.getSelected();
               if (selectedRows.length > 1) return true;
+            },
+            hidden: function () {
+              console.log(permissions)
+              return !permissions.is_creator_or_admin;
             },
             callback: function () {
               const content = '<div id="initTrackerColumnFormWrpp"></div>',
@@ -51,7 +80,7 @@ const customDropDownMenu = () => {
                             visibility: 'E',
                             metaInfo: {
                               label: 'Insert Column Left',
-                              position: (hotInstance.getSelected()[0][1] - 1),
+                              position: (hotInstance.getSelected()[0][1]),
                             },
                           };
               ES6MangoSpring.trackerModule.tracker().initFancybox(null, content, 650, () => {
@@ -75,6 +104,9 @@ const customDropDownMenu = () => {
               const selectedRows = this.getSelected();
               if (selectedRows.length > 1) return true;
             },
+            hidden: function () {
+              return !permissions.is_creator_or_admin;
+            },
             callback: function () {
               // const columnNumber = hotInstance.getSelected()[0][1],
               //       columnLength = colInfo.length;
@@ -93,7 +125,7 @@ const customDropDownMenu = () => {
                             visibility: 'E',
                             metaInfo: {
                               label: 'Insert Column Right',
-                              position: (hotInstance.getSelected()[0][1]),
+                              position: (hotInstance.getSelected()[0][1] + 1),
                             },
                           };
               ES6MangoSpring.trackerModule.tracker().initFancybox(null, content, 650, () => {
@@ -109,15 +141,24 @@ const customDropDownMenu = () => {
             disabled: function () {
               // for which cells it should be disabled
             },
+            hidden: function () {
+              return !permissions.is_creator_or_admin;
+            },
             callback: function (key, selection) {
               const colNumber = selection[0].end.col,
-                    colId = this.getCellMeta(0, colNumber).prop,
+                    colId = this.getCellMeta(0, colNumber).colId,
                     trackerId = document.querySelector('input[name="tracker_id"]').value,
                     trackerViewId = document.querySelector('input[name="tracker_view_id"]').value,
                     url = `/user/v2/tracker/tracker_columns/${colId}/destory_dialog?tracker_id=${trackerId}&tracker_view_id=${trackerViewId}`;
 
               ES6MangoSpring.trackerModule.tracker().initFancybox(url, null, 650, () => {
-                
+                document.querySelector('.hideViewColumnBtn').on('click', () => {
+                  ES6MangoSpring.trackerModule.tracker().hideColumn(hotInstance, colId, colNumber);
+                });
+
+                document.querySelector('.deleteViewColumnBtn').on('click', () => {
+                  ES6MangoSpring.trackerModule.tracker().deleteColumn(hotInstance, colId, colNumber);
+                });
               });
 
                     // url = `/user/v2/tracker/tracker_columns/${colId}/edit?tracker_id=${trackerId}`,
@@ -131,7 +172,7 @@ const customDropDownMenu = () => {
               //   colHeaders,
               // });
             },
-        }; 
+        };
         dropDownMenu['sep1'] = '---------';
         dropDownMenu['alignment'] = {
             name: '<i class="far fa-align-left lft-arw-opct"></i> Alignment <i class="far fa-chevron-right dd-rgArowTbl"></i>',
@@ -139,31 +180,13 @@ const customDropDownMenu = () => {
         dropDownMenu['hidden_columns_hide'] = {
             name: '<i class="far fa-eye-slash"></i> Hide Column',
             disabled: function () {},
+            hidden: function () {
+              return !permissions.is_creator_or_admin;
+            },
             callback: function (key, selection) {
-                const colNumber = selection[0].end.col,
-                  colId = this.getCellMeta(0, colNumber).prop,
-                  trackerId = document.querySelector('input[name="tracker_id"]').value,
-                  trackerViewId = document.querySelector('input[name="tracker_view_id"]').value,
-                  url = `/user/v2/tracker/views/${trackerViewId}/tracker_view_columns/hide_column?tracker_id=${trackerId}&tracker_column_id=${colId}`;
-
-                jQuery.ajax({
-                  url,
-                  type: 'PUT',
-                  dataType: 'JSON',
-                  success: (response) => {
-                    if (response.success) {
-                      let columArray = [];
-                      columArray = Handsontable.helper.deepClone(hotInstance.getPlugin('hiddenColumns').hiddenColumns);
-                      columArray.push(colNumber);
-                      hotInstance.updateSettings({
-                          hiddenColumns: {
-                              columns: columArray,
-                              indicators: true,
-                          },
-                      });
-                    }
-                  },
-                });
+              const colNumber = selection[0].end.col,
+                colId = this.getCellMeta(0, colNumber).colId;
+              ES6MangoSpring.trackerModule.tracker().hideColumn(hotInstance, colId, colNumber);
             },
         };
         // dropDownMenu['lockcolumn'] = {
